83cc342d8fda4d8b72462289aa86a747afc27868
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXPivotGrid_HowToHideFilterItems/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using DevExpress.Xpf.PivotGrid;
+
+namespace DXPivotGrid_HowToHideFilterItems {
+    public partial class MainWindow : Window {
+        DataSet1.ProductReportsDataTable productReportsDataTable = 
+            new DataSet1.ProductReportsDataTable();
+        DataSet1TableAdapters.ProductReportsTableAdapter productReportsDataAdapter = 
+            new DataSet1TableAdapters.ProductReportsTableAdapter();
+        public MainWindow() {
+            InitializeComponent();
+            pivotGridControl1.DataSource = productReportsDataAdapter.GetData();
+        }
+        private void pivotGridControl1_CustomFilterPopupItems(object sender, 
+                PivotCustomFilterPopupItemsEventArgs e) {
+            List<object> values = e.Field.GetVisibleValues();
+            values.Sort();
+            for (int i = e.Items.Count - 1; i >= 0; i--) {
+                if (e.Items[i].IsChecked == true && values.BinarySearch(e.Items[i].Value) < 0)
+                    e.Items.RemoveAt(i);
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to hide filter items whose corresponding field values are not displayed
+
+
+<p>The following example shows how to hide filter items whose corresponding field values are not displayed.</p><p>In this example, the Row Header Area of the PivotGrid contains two fields: 'Category Name' and 'Product Name'. If an end-user hides a particular product category via the 'Category Name' field's filter drop-down, the corresponding products will be excluded from the filter drop-down of the 'Product Name' field. To hide filter items, the CustomFilterPopupItems event is handled.</p><br />
+
+
+<br/>
+
+
